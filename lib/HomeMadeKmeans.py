cf8ddb61db08d1d173688e44b8fceac779b34457
jiahui8Zhu <jiahui.zhu1503@gmail.com>
2018-03-27 15:21:55 +0200

    Add files via upload

--- lib/HomeMadeKmeans.py
+++ lib/HomeMadeKmeans.py
@@ -15,14 +15,14 @@ import matplotlib.pyplot as plt
 # In[262]:
 
 
-def get_distance(point_data,points_K):
+def get_distance(point_data,points_k):
     dist=[]
-    for i in points_K:
+    for i in points_k:
         dist.append(math.sqrt(sum([(a-b)**2 for a,b in zip(point_data,i)])))
     return dist
 
 
-# In[263]:
+
 
 
 def random_points(k,df):
@@ -30,7 +30,7 @@ def random_points(k,df):
     for z in range(k):
         point = []
         for i in range(df.shape[1]-1):
-            point.append(np.random.randint(df.iloc[:,i].min(),df.iloc[:,i].max()))
+            point.append(np.random.randint(df.iloc[:,i].min()-1,df.iloc[:,i].max()))
         points.append(point)
     return points
 
@@ -38,9 +38,9 @@ def random_points(k,df):
 # In[264]:
 
 
-def get_new_pointsK(df,k):
+def get_new_pointsk(df,k):
     points_k =[]
-    for i in range(K): # nb point = K
+    for i in range(k): # nb point = k
         df_cluster = (df.loc[df['cluster'] == i])['point_data']
 
         size = len(df_cluster)
@@ -52,7 +52,7 @@ def get_new_pointsK(df,k):
             for h in df_cluster:
                 liste_var[j] += h[j]
         point = ([x/df_cluster.shape[0] for x in liste_var])
-        print(point)
+      
         points_k.append(point)  
     return points_k  
 
@@ -65,14 +65,17 @@ def kmeans (itr,k,df):
     for i in range(itr) :
         if i == 0:
             points_k = random_points(k,df)
-            print(points_k)
+            liste_col = df.columns
+            df['point_data'] = df[liste_col].apply(tuple, axis=1)
+            
+        
             df['distance'] = df['point_data'].apply(lambda x: get_distance(x,points_k))
             df['cluster'] = df['distance'].apply(lambda x : np.argmin(x))
             
         else:
         
-            points_k = get_new_pointsK(df,k)
-            print(points_k)
+            points_k = get_new_pointsk(df,k)
+       
             df['distance'] = df['point_data'].apply(lambda x: get_distance(x,points_k))
             df['cluster'] = df['distance'].apply(lambda x : np.argmin(x))
           
